09863628385d1050ca04da202c3803528f3e2b8a
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-01-28 07:39:25 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # repo init
-repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+repo init -u https://github.com/VoltageOS/manifest.git -b 16.2 --git-lfs
 
 # repo sync script
 /opt/crave/resync.sh
@@ -7,51 +7,20 @@ repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --n
 # Remove old device specific repos
 remove=(
 device/xiaomi
-kernel/xiaomi
-vendor/xiaomi
-hardware/xiaomi
-packages/resources/devicesettings
 vendor/lineage-priv/keys
 )
 
 rm -rf "${remove[@]}"
 
 # Deivce Trees
-git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch -b aosp-16 device/xiaomi/munch
-git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common.git -b aosp-16-qpr2 device/xiaomi/sm8250-common
-
-# Vendor Trees
-git clone https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_munch vendor/xiaomi/munch
-git clone https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_sm8250-common -b aosp-16-qpr2 vendor/xiaomi/sm8250-common
-
-# Munch Firmware
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware
-
-# Kernel Tree
-git clone https://github.com/Olzhas-Kdyr/kernel_sm8250 -b aosp-16-qpr1 kernel/xiaomi/sm8250
-
-# Hardware Xiaomi
-git clone https://github.com/crdroidandroid/android_hardware_xiaomi -b 16.0 hardware/xiaomi
-
-# MIUI Camera
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
-
-# devicesettings
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings
+git clone https://github.com/ryznstk/device_xiaomi.git -b vos device/xiaomi/peridot
 
 # My Keys
-git clone https://github.com/Olzhas-Kdyr/keys vendor/lineage-priv/keys
-
-cd kernel/xiaomi/sm8250
-bash ksu.sh
-rm -rf KernelSU-Next/userspace/su
-cd ../../..
+git clone https://github.com/droidcore/private_key.git vendor/lineage-priv/keys
 
 # Building 
 . build/envsetup.sh
-export BUILD_USERNAME=olzhas
+export BUILD_USERNAME=BLU
 export BUILD_HOSTNAME=ubuntu
-export SKIP_ABI_CHECKS=true
 make installclean
-lunch lineage_munch-bp4a-user
-m bacon
+brunch peridot
